0d3c5462f3c866b481506bfee28429a243ea91fd
anuraag <anuraag.warudkar@gmail.com>
2023-10-13 00:51:48 -0400

    added problem 3 (naive bayes) and report started

--- hw3/main.py
+++ hw3/main.py
@@ -3,7 +3,149 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-def readCSV(filepath, scaler="nrml"):
+
+def readCSV(filepath, dtypes, output, scaler="nrml", replace=None, remove=[]):
+    df = pd.read_csv(filepath, dtype=dtypes)
+    outcome = df.pop(output)
+    if scaler == "nrml":
+        df = df.apply(lambda x: (x - x.min()) / (x.max() - x.min()))
+    else:
+        means = df.mean()
+        stds = df.std()
+        df = (df - means) / stds
+
+    if replace is not None:
+        outcome = outcome.replace(replace)
+    if remove:
+        for col in remove:
+            del df[col]
+
+    return df, outcome
+
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+
+def predict(X, theta, bias):
+    linear_model = np.dot(X, theta) + bias
+    return sigmoid(linear_model)
+
+
+def logisticRegression(X, y, learningRate=0.1, iterations=1000, tolerance=1e-4, strength=0.1, reg=""):
+    num_samples, num_features = X.shape
+
+    theta = np.zeros(num_features)
+    bias = 0
+    previous_cost = float("inf")  # Initialize with a large value
+    costs = []
+
+    for i in range(iterations):
+        model = np.dot(X, theta) + bias
+        predictions = sigmoid(model)
+        predictions = np.clip(predictions, 1e-15, 1-1e-15)
+
+        cost = -1/num_samples * \
+            np.sum(y * np.log(predictions) + (1 - y) * np.log(1 - predictions))
+
+        if reg == "l1":
+            cost += strength * np.sum(np.abs(theta))
+        elif reg == "l2":
+            cost += strength * np.sum(np.square(theta))
+
+        costs.append(cost)
+
+        dw = (1 / num_samples) * np.dot(X.T, (predictions - y))
+
+        if reg == "l1":
+            dw += strength * np.sign(theta)
+        elif reg == "l2":
+            dw += 2 * strength * theta
+        db = (1 / num_samples) * np.sum(predictions - y)
+
+        theta -= learningRate * dw
+        bias -= learningRate * db
+
+        if i > 0 and abs(previous_cost - cost) < tolerance:
+            break
+
+        previous_cost = cost
+
+    plt.plot(np.arange(i+1), costs)
+    plt.xlabel("Epoch")
+    plt.ylabel("Binary Cross Entropy Loss")
+    plt.title(f"Loss over {i} epochs")
+    plt.show()
+
+    return theta, bias
+
+
+def plotFeatures(featureNames, theta):
+    plt.bar(featureNames, theta)
+    plt.xlabel("Features")
+    plt.ylabel("Coefficients (Weights)")
+    plt.title("Feature Importance")
+    plt.show()
+
+
+def accuracy_(predicted, y):
+    # Calculate accuracy
+    correct_predictions = np.sum(predicted == y)
+    total_predictions = len(y)
+    accuracy = correct_predictions / total_predictions
+    return accuracy
+
+
+def precision_(matrix):
+    tp = matrix[0][0]
+    fp = matrix[1][0]
+
+    return round(tp / (tp + fp), 4)
+
+
+def recall_(matrix):
+    tp = matrix[0][0]
+    fn = matrix[1][1]
+
+    return round(tp / (tp + fn), 4)
+
+
+def f1_(precision, recall):
+    return round((2 * precision * recall) / (precision + recall), 4)
+
+
+def confusionMatrix(predicted, y):
+    tp = np.sum(predicted == y)
+    tn = sum(predicted != y)
+    fp = sum(1 for true_label, predicted_label in zip(y, predicted) if predicted_label == 1 and true_label == 0)
+    fn = sum(1 for true_label, predicted_label in zip(y, predicted) if predicted_label == 0 and true_label == 1)
+
+    return [[tp, tn], [fp, fn]]
+
+class NaiveBayesClassifier:
+    def __init__(self):
+        self.mean = {}
+        self.var = {}
+        self.priors = {}
+    
+    def fit(self, X, y):
+        unique_classes = np.unique(y)
+        for label in unique_classes:
+            X_class = X[y == label]
+            self.mean[label] = X_class.mean(axis=0)
+            self.var[label] = X_class.var(axis=0)
+            self.priors[label] = float(len(X_class) / len(X))
+    
+    def predict(self, X):
+        probs = np.zeros((len(X), len(self.priors)))
+        for label, prior in self.priors.items():
+            probs[:, label] = np.log(prior) + \
+                              -0.5 * np.sum(np.log(2 * np.pi * self.var[label])) - \
+                              0.5 * np.sum(((X - self.mean[label]) ** 2) / (self.var[label]), axis=1)
+        return np.argmax(probs, axis=1)
+
+def problem1():
+    scaler = "std"
     dtypes = {
         'Pregnancies': int,
         'Glucose': int,
@@ -15,78 +157,191 @@ def readCSV(filepath, scaler="nrml"):
         'Age': int,
         'Outcome': int
     }
-    df = pd.read_csv(filepath, dtype=dtypes)
+    df, outcome = readCSV("./datasets/diabetes.csv",
+                          scaler=scaler, dtypes=dtypes, output="Outcome")
+    df_valid, outcome_valid = readCSV(
+        "./datasets/diabetes-valid.csv", scaler=scaler, dtypes=dtypes, output="Outcome")
 
-    outcome = df.pop("Outcome")
+    # problem 1 - diabetes classification
+    theta, bias = logisticRegression(
+        df, outcome, learningRate=0.01, strength=0.1, reg="")
 
-    if scaler == "normalize":
-        df = df.apply(lambda x: (x - x.min()) / (x.max() - x.min()))
-    else:
-        means = df.mean()
-        stds = df.std()
-        df = (df - means) / stds
-
-    return df, outcome
-
-def sigmoid(z):
-    return 1 / (1 + np.exp(-z))
-
-def predict(X, theta, bias):
-    linear_model = np.dot(X, theta) + bias
-    return sigmoid(linear_model)
-
-def evaluate_accuracy(X, y, theta, bias):
     # Make predictions on the validation data
-    y_pred = predict(X, theta, bias)
-    
-    # Threshold the predictions (e.g., >= 0.5 as 1, < 0.5 as 0)
-    y_pred = np.round(y_pred)
-    
-    # Calculate accuracy
-    correct_predictions = np.sum(y_pred == y)
-    total_predictions = len(y)
-    accuracy = correct_predictions / total_predictions
-    
-    return accuracy
+    predicted = predict(df_valid, theta, bias)
+    predicted = np.round(predicted)
 
-def logisticRegression(X, y, learningRate=0.1, iterations = 300):
-    num_samples, num_features = X.shape
-    
-    theta = np.zeros(num_features)
-    bias = 0
+    # plot confusion matrix
+    confMatrix = confusionMatrix(predicted, outcome_valid)
 
-    for i in range(iterations):
-        model = np.dot(X, theta) + bias
-        predictions = sigmoid(model)
+    print("\nConfusion Matrix:")
+    for row in confMatrix:
+        print(row)
 
-        dw = (1 / num_samples) * np.dot(X.T, (predictions - y))
-        db = (1 / num_samples) * np.sum(predictions - y)
+    # accuracy metrics
+    accuracy = accuracy_(predicted, outcome_valid)
+    precision = precision_(confMatrix)
+    recall = recall_(confMatrix)
+    f1 = f1_(precision, recall)
 
-        theta -= learningRate * dw
-        bias -= learningRate * db
-    
-    return theta, bias
+    print(f"\nAccuracy: {round(accuracy*100,4)}%")
+    print(f"Precision: {precision*100}%")
+    print(f"Recall: {recall*100}%")
+    print(f"F1: {f1}")
 
-def plotFeatures(featureNames, theta):
-    plt.bar(featureNames, theta)
-    plt.xlabel("Features")
-    plt.ylabel("Coefficients (Weights)")
-    plt.title("Feature Importance")
-    plt.show()
+    plotFeatures(["preg", "glucose", "bp", "skinthick",
+                 "insulin", "bmi", "dpf", "age"], theta)
+
+
+def problem2():
+    scaler = "nrml"
+    dtypes = {
+        'id': int,
+        'diagnosis': str,
+        'radius_mean': float,
+        'texture_mean': float,
+        'perimeter_mean': float,
+        'area_mean': float,
+        'smoothness_mean': float,
+        'compactness_mean': float,
+        'concavity_mean': float,
+        'concave points_mean': float,
+        'symmetry_mean': float,
+        'fractal_dimension_mean': float,
+        'radius_se': float,
+        'texture_se': float,
+        'perimeter_se': float,
+        'area_se': float,
+        'smoothness_se': float,
+        'compactness_se': float,
+        'concavity_se': float,
+        'concave points_se': float,
+        'symmetry_se': float,
+        'fractal_dimension_se': float,
+        'radius_worst': float,
+        'texture_worst': float,
+        'perimeter_worst': float,
+        'area_worst': float,
+        'smoothness_worst': float,
+        'compactness_worst': float,
+        'concavity_worst': float,
+        'concave points_worst': float,
+        'symmetry_worst': float,
+        'fractal_dimension_worst': float
+    }
+    replace = {
+        "B": 1,
+        "M": 0
+    }
+    df, outcome = readCSV("./datasets/cancer.csv", scaler=scaler,
+                          dtypes=dtypes, output="diagnosis", replace=replace, remove=["id"])
+    df_valid, outcome_valid = readCSV("./datasets/cancer-valid.csv", scaler=scaler,
+                                      dtypes=dtypes, output="diagnosis", replace=replace, remove=["id"])
+
+    theta, bias = logisticRegression(
+        df, outcome, learningRate=0.1, strength=0.01, reg="l1")
+
+    predicted = predict(df_valid, theta, bias)
+    predicted = np.round(predicted)
+
+    confMatrix = confusionMatrix(predicted, outcome_valid)
+
+    print("\nConfusion Matrix:")
+    for row in confMatrix:
+        print(row)
+
+    accuracy = accuracy_(predicted, outcome_valid)
+    precision = precision_(confMatrix)
+    recall = recall_(confMatrix)
+    f1 = f1_(precision, recall)
+
+    print(f"\nAccuracy: {round(accuracy*100,4)}%")
+    print(f"Precision: {precision*100}%")
+    print(f"Recall: {round(recall*100,4)}%")
+    print(f"F1: {f1}")
+
+
+def problem3():
+    scaler = "nrml"
+    dtypes = {
+        'id': int,
+        'diagnosis': str,
+        'radius_mean': float,
+        'texture_mean': float,
+        'perimeter_mean': float,
+        'area_mean': float,
+        'smoothness_mean': float,
+        'compactness_mean': float,
+        'concavity_mean': float,
+        'concave points_mean': float,
+        'symmetry_mean': float,
+        'fractal_dimension_mean': float,
+        'radius_se': float,
+        'texture_se': float,
+        'perimeter_se': float,
+        'area_se': float,
+        'smoothness_se': float,
+        'compactness_se': float,
+        'concavity_se': float,
+        'concave points_se': float,
+        'symmetry_se': float,
+        'fractal_dimension_se': float,
+        'radius_worst': float,
+        'texture_worst': float,
+        'perimeter_worst': float,
+        'area_worst': float,
+        'smoothness_worst': float,
+        'compactness_worst': float,
+        'concavity_worst': float,
+        'concave points_worst': float,
+        'symmetry_worst': float,
+        'fractal_dimension_worst': float
+    }
+    replace = {
+        "B": 1,
+        "M": 0
+    }
+    df, outcome = readCSV("./datasets/cancer.csv", scaler=scaler,
+                          dtypes=dtypes, output="diagnosis", replace=replace, remove=["id"])
+    df_valid, outcome_valid = readCSV("./datasets/cancer-valid.csv", scaler=scaler,
+                                      dtypes=dtypes, output="diagnosis", replace=replace, remove=["id"])
+
+    clf = NaiveBayesClassifier()
+    clf.fit(df, outcome)
+
+    # Predict on the validation set
+    predictions = clf.predict(df_valid)
+
+    confMatrix = confusionMatrix(predictions, outcome_valid)
+
+    print("\nConfusion Matrix:")
+    for row in confMatrix:
+        print(row)
+
+    accuracy = accuracy_(predictions, outcome_valid)
+    precision = precision_(confMatrix)
+    recall = recall_(confMatrix)
+    f1 = f1_(precision, recall)
+
+    print(f"\nAccuracy: {round(accuracy*100,4)}%")
+    print(f"Precision: {precision*100}%")
+    print(f"Recall: {round(recall*100,4)}%")
+    print(f"F1: {f1}")
+
+
+def problem4():
+    pass
+
+
+def problem5():
+    pass
+
 
 def main():
-    df, outcome = readCSV("./diabetes.csv", scaler="std")
-    df_valid, outcome_valid = readCSV("./diabetes-valid.csv")
+    # problem1()
 
-    theta, bias = logisticRegression(df, outcome, learningRate=0.01)
-    accuracy = evaluate_accuracy(df_valid, outcome_valid, theta, bias)
-    
-    print(theta, bias, f"{round(accuracy*100,4)}% accuracy")
+    # problem2()
 
-    features = ["preg", "glucose", "bp", "skintick", "insulin", "bmi", "dpf", "age"]
-
-    plotFeatures(features, theta)
-
+    problem3()
 
 
 if __name__ == "__main__":
