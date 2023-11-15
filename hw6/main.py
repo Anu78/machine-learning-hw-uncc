507d551d1188273c2a13eb8c09f5c9ce312dd01b
anuraag <anuraag.warudkar@gmail.com>
2023-11-15 16:30:40 -0500

    templated project and hw6

---
+++ hw6/main.py
@@ -0,0 +1,17 @@
+import torch
+
+"""
+Create a fully connected neural network to predict values based on values from the housing dataset.
+"""
+
+
+
+"""
+Create another fully connected neural network to predict image classes based on the CIFAR-10 dataset 
+"""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
