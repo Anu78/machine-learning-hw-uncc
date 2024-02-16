25d54a5282cce03413a12415b0f74004dbfc9bbe
anuraag <anuraag.warudkar@gmail.com>
2024-02-16 17:31:30 -0500

    finished resnet18 impl

--- deep-learning/hw2/main.py
+++ deep-learning/hw2/main.py
@@ -1,6 +1,4 @@
 import torch
-from torch._dynamo.utils import identity
-from torch.nn.modules import dropout
 from torch.utils.data import DataLoader
 from torchvision import transforms 
 import torchvision.datasets
@@ -67,15 +65,67 @@ class Block(nn.Module):
         if self.identDownsample is not None: 
             identity = self.identDownsample(identity) 
 
-class ResNet18(nn.Module): 
-    def __init__(self): 
-        super(ResNet18).__init__()
-        pass 
-    def forward(self, x): 
-        pass
+        x += identity
+        x = self.relu(x)
+        return x
 
+class ResNet_18(nn.Module):
+    
+    def __init__(self, image_channels, num_classes):
+        
+        super(ResNet_18, self).__init__()
+        self.in_channels = 64
+        self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=7, stride=2, padding=3)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        
+        #resnet layers
+        self.layer1 = self.__make_layer(64, 64, stride=1)
+        self.layer2 = self.__make_layer(64, 128, stride=2)
+        self.layer3 = self.__make_layer(128, 256, stride=2)
+        self.layer4 = self.__make_layer(256, 512, stride=2)
+        
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512, num_classes)
+        
+    def __make_layer(self, in_channels, out_channels, stride):
+        
+        identity_downsample = None
+        if stride != 1:
+            identity_downsample = self.identity_downsample(in_channels, out_channels)
+            
+        return nn.Sequential(
+            Block(in_channels, out_channels, identDownsample=identity_downsample, stride=stride), 
+            Block(out_channels, out_channels)
+        )
+        
+    def forward(self, x):
+        
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        
+        x = self.avgpool(x)
+        x = x.view(x.shape[0], -1)
+        x = self.fc(x)
+        return x 
+    
+    def identity_downsample(self, in_channels, out_channels):
+        
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=2, padding=1), 
+            nn.BatchNorm2d(out_channels)
+        )
 
 def train(model, epochs, trainloader, testloader):
+
     model.to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
@@ -84,7 +134,7 @@ def train(model, epochs, trainloader, testloader):
         model.train() 
         runningLoss = 0
 
-        for i, data in enumerate(trainloader, 1):
+        for data in trainloader:
             inputs, labels = data[0].to(device), data[1].to(device)
 
             optimizer.zero_grad()
@@ -138,12 +188,15 @@ def part1(epochs):
     train(model_no_dropout, epochs, trainloader, testloader)
 
 
-def part2():
+def part2(epochs):
     """
     build a resnet-18 and train on cifar. compare with results from resnet-11 and   
     experiment with dropout for both networks
     """
     transform = transforms.Compose([
+        transforms.ToTensor(), 
+        transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))
+        ])
 
     trainset = torchvision.datasets.CIFAR10(root="../datasets", train=True, download=True, transform=transform)
 
@@ -153,7 +206,12 @@ def part2():
     
     testloader = DataLoader(testset, batch_size=4, shuffle=True, num_workers=2)
 
-    # start model training
+    # start model training - define model
+    model = ResNet_18(1, 10) 
+
+    print("training resnet18 model")
+    train(model, epochs, trainloader, testloader)
+    
 
 def main():
     a = sys.argv
@@ -163,10 +221,10 @@ def main():
         print("incorrect number of args")
         return
 
-    if a[1] == "1": 
+    if a[1] == '1': 
         part1(int(a[2])) 
-    else: 
-        part2() 
+    elif a[1] == '2': 
+        part2(int(a[2])) 
 
 if __name__ == "__main__":
     main()
